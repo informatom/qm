58c1a60abecd5f9c89dad37bbde94863d7c862cf
Stefan Haslinger <stefan.haslinger@mittenin.at>
2012-09-15 12:12:40 +0200

    Basis-Routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Qm::Application.routes.draw do
   devise_for :users
   resources :users
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -52,11 +49,7 @@ Qm::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'users#index'
 
   # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
