640a9dad1b9b9e54347e633bc3796e561ee95c00
Stefan Haslinger <stefan.haslinger@mittenin.at>
2012-10-04 13:18:54 +0200

    Mailing aktiviert

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ Qm::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  ### Stefan: 05.04.12 - 14:20 ###: für Devise nötig
+  config.action_mailer.default_url_options = { :host => 'www.informatom.com' }
 end
