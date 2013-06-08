315abde9600b257dfd328b4c8d1c74b9ce2257ae
Stefan Haslinger <stefan.haslinger@mittenin.at>
2013-06-08 12:56:55 +0200

    diverse Zuordnungen

--- app/models/business_process.rb
+++ app/models/business_process.rb
@@ -3,7 +3,11 @@ class BusinessProcess < ActiveRecord::Base
   has_paper_trail
 
   attr_accessible :aims, :ancestry, :company_id, :description, :name, :number, :owner_id, :parent_id, :process_class_id, :purpose,
-                  :x, :y, :area
+                  :x, :y, :area,
+                  :business_process_note_assignments_attributes,
+                  :business_process_document_assignments_attributes,
+                  :business_process_process_indicator_assignments_attributes,
+                  :business_process_department_assignments_attributes
 
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => :company_id
@@ -15,15 +19,19 @@ class BusinessProcess < ActiveRecord::Base
 
   has_many :business_process_note_assignments, :dependent => :restrict
   has_many :notes, :through => :business_process_note_assignments, :dependent => :restrict
+  accepts_nested_attributes_for :business_process_note_assignments, :allow_destroy => true
 
   has_many :business_process_document_assignments, :dependent => :restrict
   has_many :documents, :through => :business_process_document_assignments, :dependent => :restrict
+  accepts_nested_attributes_for :business_process_document_assignments, :allow_destroy => true
 
   has_many :business_process_process_indicator_assignments, :dependent => :restrict
   has_many :process_indicators, :through => :business_process_process_indicator_assignments, :dependent => :restrict
+  accepts_nested_attributes_for :business_process_process_indicator_assignments, :allow_destroy => true
 
   has_many :business_process_department_assignments, :dependent => :restrict
   has_many :departments, :through => :business_process_department_assignments, :dependent => :restrict
+  accepts_nested_attributes_for :business_process_department_assignments, :allow_destroy => true
 
   has_many :process_steps
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,7 @@
 class Note < ActiveRecord::Base
-  attr_accessible :company_id, :description, :name, :duration, :location, :medium, :archiving
+  attr_accessible :company_id, :description, :name, :duration, :location, :medium, :archiving,
+                  :business_process_note_assignments_attributes,
+                  :note_process_step_assignments_attributes
 
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => :company_id
@@ -8,7 +10,9 @@ class Note < ActiveRecord::Base
 
   has_many :business_process_note_assignments, :dependent => :restrict
   has_many :business_processes, :through => :business_process_note_assignments, :dependent => :restrict
+  accepts_nested_attributes_for :business_process_note_assignments, :allow_destroy => true
 
   has_many :note_process_step_assignments, :dependent => :restrict
   has_many :process_steps, :through => :note_process_step_assignments, :dependent => :restrict
+  accepts_nested_attributes_for :note_process_step_assignments, :allow_destroy => true
 end
