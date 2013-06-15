779f9c95fe87f8efc5ee16cd3a3d5da2f5532eb0
Stefan Haslinger <stefan.haslinger@mittenin.at>
2013-06-15 13:53:07 +0200

    Kontaktaufnahme5

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -3,9 +3,7 @@ class SubmissionsController < ApplicationController
 
   def create
     @submission = Submission.new(params[:submission])
-    @testimonials = Paragraph.where( :assignment => "Kontakt", :position => 1).first
-    if @submission.save
-      UserMailer.submission(@submission).deliver
+    if UserMailer.submission(@submission).deliver
       redirect_to root_path, :notice => 'E-Mail wurde erfolgreich erzeugt. Wir werden Sie kontaktieren.'
     else
       redirect_to root_path, :notice => 'E-mail konnte nicht gesendet werden. Ist Ihre E-Mailadresse korrekt?'
