6b232778261dac5f207fd04f7aeab1a6a1c35d2a
Stefan Haslinger <stefan.haslinger@mittenin.at>
2014-02-25 11:45:43 +0100

    inverse relations fixed

--- app/models/process_step.rb
+++ app/models/process_step.rb
@@ -22,20 +22,20 @@ class ProcessStep < ActiveRecord::Base
   belongs_to :updator, :class_name => "User", foreign_key: "updated_by", :inverse_of => :updated_process_steps
   belongs_to :flow_object
 
-  has_many :successors, :through => :outgoing_sequence_flows, :dependent => :restrict, 
-           :source => :target, :inverse_of => :process_step
+  has_many :successors, :through => :outgoing_sequence_flows, :dependent => :restrict,
+           :source => :target, :inverse_of => :predecessors
   has_many :outgoing_sequence_flows, :class_name => "SequenceFlow", :dependent => :restrict, :foreign_key => "source_id",
            :dependent => :restrict, :inverse_of => :source
   accepts_nested_attributes_for :outgoing_sequence_flows, :allow_destroy => true
 
-  has_many :predecessors, :through => :incoming_sequence_flows, :dependent => :restrict, 
-           :source => :source, :inverse_of => :process_step
+  has_many :predecessors, :through => :incoming_sequence_flows, :dependent => :restrict,
+           :source => :source, :inverse_of => :successors
   has_many :incoming_sequence_flows, :class_name => "SequenceFlow", :dependent => :restrict, :foreign_key => "target_id",
            :dependent => :restrict, :inverse_of => :target
   accepts_nested_attributes_for :incoming_sequence_flows, :allow_destroy => true
 
   has_many :instruction_process_step_assignments, :dependent => :restrict, :inverse_of => :process_step
-  has_many :instructions, :through => :instruction_process_step_assignments, :dependent => :restrict, 
+  has_many :instructions, :through => :instruction_process_step_assignments, :dependent => :restrict,
            :inverse_of => :process_steps
   accepts_nested_attributes_for :instruction_process_step_assignments, :allow_destroy => true
 
@@ -44,7 +44,7 @@ class ProcessStep < ActiveRecord::Base
   accepts_nested_attributes_for :note_process_step_assignments, :allow_destroy => true
 
   has_many :document_process_step_assignments, :dependent => :restrict, :inverse_of => :process_step
-  has_many :documents, :through => :document_process_step_assignments, :dependent => :restrict, 
+  has_many :documents, :through => :document_process_step_assignments, :dependent => :restrict,
            :inverse_of => :process_steps
   accepts_nested_attributes_for :document_process_step_assignments, :allow_destroy => true
 end
